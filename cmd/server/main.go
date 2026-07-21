a7a71099ed9c68ccf472238942189d2429b83a5d
Ilya <arkhipilya@gmail.com>
2026-07-21 22:47:39 +0300

    refactor: main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
-	"github.com/elijaharch/mentorship-task-golang/internal/httpserver"
+	"github.com/elijaharch/mentorship-task-golang/internal/server"
 	"github.com/elijaharch/mentorship-task-golang/pkg/config"
+	"github.com/elijaharch/mentorship-task-golang/pkg/logger"
 )
 
 func main() {
 	cfg := config.Load()
 
+	log := logger.New(os.Stdout, cfg.Log.Level, cfg.Log.Format)
+	slog.SetDefault(log)
+
 	router := http.NewServeMux()
 
-	srv := httpserver.New(":"+cfg.Port, router, cfg.Server)
+	srv := server.New(cfg.Server, router)
 
 	if err := srv.Run(); err != nil {
-		log.Fatalf("server failed: %v", err)
+		slog.Error("server failed", "err", err)
 	}
 }
