73ff2a500ca1dce0e262acb4e97ef94ccd770928
Ilya <arkhipilya@gmail.com>
2026-07-28 03:41:46 +0300

    refactor(handler): move errors

--- internal/handler/json.go
+++ internal/handler/json.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"io"
 	"net/http"
+
+	calculation "github.com/elijaharch/mentorship-task-golang/internal/domain"
 )
 
 const maxRequestBodyBytes = 1 << 20
@@ -36,4 +38,85 @@ func decodeJSON(w http.ResponseWriter, r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (h *Handler) writeError(
+	w http.ResponseWriter,
+	r *http.Request,
+	status int,
+	code string,
+	message string,
+) {
+	response := errorResponse{
+		Error: errorDetail{
+			Code:    code,
+			Message: message,
+		},
+	}
+
+	if err := writeJSON(w, status, response); err != nil {
+		h.logger.ErrorContext(
+			r.Context(),
+			"write error response",
+			"err",
+			err,
+		)
+	}
+}
+
+func (h *Handler) writeServiceError(
+	w http.ResponseWriter,
+	r *http.Request,
+	action string,
+	err error,
+) {
+	switch {
+	case errors.Is(err, calculation.ErrNotFound):
+		h.writeError(
+			w,
+			r,
+			http.StatusNotFound,
+			"not_found",
+			err.Error(),
+		)
+	case errors.Is(err, calculation.ErrInvalidOperation):
+		h.writeError(
+			w,
+			r,
+			http.StatusUnprocessableEntity,
+			"invalid_operation",
+			err.Error(),
+		)
+	case errors.Is(err, calculation.ErrDivisionByZero):
+		h.writeError(
+			w,
+			r,
+			http.StatusUnprocessableEntity,
+			"division_by_zero",
+			err.Error(),
+		)
+	case errors.Is(err, calculation.ErrInvalidNumber):
+		h.writeError(
+			w,
+			r,
+			http.StatusUnprocessableEntity,
+			"invalid_number",
+			err.Error(),
+		)
+	default:
+		h.logger.ErrorContext(
+			r.Context(),
+			action+" failed",
+			"err",
+			err,
+		)
+
+		h.writeError(
+			w,
+			r,
+			http.StatusInternalServerError,
+			"internal_error",
+			"internal server error",
+		)
+	}
 }
